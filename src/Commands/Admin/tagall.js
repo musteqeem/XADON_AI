610a172d02732707f617bb5255246f7a622e2fa0
musteqeem <officialxadonai@gmail.com>
2026-08-31 08:04:58 +0100

    XADON AI: Transfer Commands/Admin/tagall.js

--- src/Commands/Admin/tagall.js
+++ src/Commands/Admin/tagall.js
@@ -4,7 +4,8 @@ module.exports = {
     desc: 'Mention all group members',
     category: 'Admin',
     groupOnly: true,
-    adminOnly: true,
+    adminOnly: false,
+     // ⭐ Reaction config
     reactions: {
         start: '💬',
         success: '📢'
@@ -12,8 +13,8 @@ module.exports = {
     
     execute: async (sock, m, { text, groupMeta }) => {
         const members = groupMeta.participants.map(p => p.id);
-        const msg = text || '✪ EVERY֎NE';
-        const tags = members.map(p => `@${p.split('@')[0]}`).join('\n');
+        const msg = text || '`ⓘ EVERYONE!!`';
+        const tags = members.map(p => `⊘ @${p.split('@')[0]}`).join('\n');
         await sock.sendMessage(m.chat, { text: `${msg}\n\n${tags}`, mentions: members }, { quoted: m });
     }
 };
