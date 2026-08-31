d621667b060be80c21e9484ec1f7b9ada3d8fe68
musteqeem <officialxadonai@gmail.com>
2026-08-31 08:08:01 +0100

    XADON AI: Transfer Commands/Bot/xdnMsg.js

--- src/Commands/Bot/xdnMsg.js
+++ src/Commands/Bot/xdnMsg.js
@@ -134,7 +134,7 @@ const handleMessage = async (sock, m, store) => {
         console.log(chalk.red('[MSG ERROR]'), err.message);
 
         sock.sendMessage(m.chat, {
-            react: { text: '🥇', key: m.key }
+            react: { text: '🙈', key: m.key }
         }).catch(() => {});
     }
 };
