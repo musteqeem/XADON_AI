8798331476856b66e35dbcc3321a62483118fd64
musteqeem <officialxadonai@gmail.com>
2026-09-01 09:04:15 +0100

    XADON AI: Transfer Api.js

---
+++ library/Api.js
@@ -0,0 +1,26 @@
+
+// © 2026 MUSTEQEEM. All Rights Reserved.
+// respect the work, don’t just copy-paste.
+
+const config = require('../settings/config');
+const fetch = require('node-fetch')
+
+const Api = {
+  get: async (endpoint, params = {}) => {
+    const query = new URLSearchParams(params).toString()
+    const res = await fetch(`${config.api.baseurl}${endpoint}?${query}`)
+    return await res.json()
+  },
+
+  post: async (endpoint, body = {}) => {
+    const res = await fetch(`${config.api.baseurl}${endpoint}`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(body)
+    })
+    return await res.json()
+  }
+}
+
+module.exports = Api
+
