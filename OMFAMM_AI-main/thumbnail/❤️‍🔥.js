e26ed10270b3efd9cf5381d4f821d862fcfa3a2a
musteqeem <officialxadonai@gmail.com>
2026-09-12 17:49:59 +0100

    Upload OMFAMM_AI-main/thumbnail/❤️‍🔥.js

---
+++ OMFAMM_AI-main/thumbnail/❤️‍🔥.js
@@ -0,0 +1,147 @@
+//
+//℘𒆜𓀀∞⌘☽☼✗✐◉❂𓅓𓊈𝑽꯭𝑰꯭𝑷ࠡࠡࠡࠡࠢ𓊉■☉✧◢◤◥◣彡㉨⁠♪⁠ಥ⁠‿⁠ಥ)(⁠☞⁠ ͡⁠°⁠ ͜⁠ʖ⁠ ͡⁠°⁠)⁠☞ᄒ⁠ᴥ⁠ᄒ⁠◈❏◦⋆⁩⁩ ♡ ㅤ  ⎙ㅤ  ⌲            
+//ˡᶦᵏᵉ   ˢᵃᵛᵉ  ˢʰᵃʳᵉ.‎✦✯𓉤𓄄✘⚉☬乂亗✪ ✦ ,  ✪ ,  ⚉ ,  ★ ,  ✯ 
+//✧ ✩ ✪ ✫ ✬ ✭ ✮ ✯ ✰ ✱
+//✵ ✶ ✷ ✸ ✹ ✺ ✻ ✼ ✽ ✾
+//✿ ❀ ❁ ❃ ❄ ❅ ❆ ❇ ❈ ❉
+//❊ ❋ ❍ ❑ ❒ ❓ ❔ ❕ ❖ ❗
+//∗ ⍟ ⍣ ⍤ ⍥ ⍦ ⍧ ⍨ ⍩ ⍪
+//♕ ♖ ♗ ♘ ♙ ♚ ♛ ♜ ♝ ♞
+//✠ ✡ ✪ ⎔ ⏚ ⌘ ⌥ ⎈ ⍟ ∆
+//◐ ◑ ◒ ◓ ◔ ◕ ◖ ◗ ◙ ◚
+
+/**.welcome Welcome 🤗 
+
+𝌆```Tutoral```
+https://youtu.be/duOK_pdIo6M?si=7508HE9JqQnQ2U7p
+
+⎔```Deploy Site```
+
+https://web.crysnovax.link
+
+ஃ𖠃```FREE PANEL```
+
+https://leonodes.xyz/login?ref=6238a049
+
+𓀀```Support Group```
+
+https://chat.whatsapp.com/Besbj8VIle1GwxKKZv1lax?mode=gi_t
+
+🜲```Repo```
+https://github.com/crysnovax/CRYSNOVA_AI 
+
+
+`ⓘ Don't forget to ✯ my repo`
+
+( ͡❛ ₃ ͡❛)
+*/
+
+/*
+░░▓▓░░▓▓░  *CRYSN⎔VA AI* ░▓▓░░▓▓░░
+           ░░▓▓░░▓░             ░▓▓░░▓░
+   
+
+ꜱᴩᷨᴇͦᴇͭᴅ
+                             ﮩ٨ـﮩﮩ٨ـ♡ﮩ٨ـﮩﮩ٨ـ
+
+━━━━━━━━━━━━━━━━━━
+
+⚉ Welcome to CRYSNOVA AI Updates
+
+```㋛ Pair Site```
+
+sl.crysnovax.link/pair
+
+```𝌆 Tutorials```
+
+*➊*  sl.crysnovax.link/tutorial1
+*➋*  sl.crysnovax.link/tutorial2
+
+```彡 Community```
+
+sl.crysnovax.link/WHATSAPP
+
+```▢ Telegram```
+
+Channel: t.me/CRYSNOVA_AI
+Group: t.me/CODY_CH
+
+━━━━━━━━━━━━━━━━━━
+
+```☁︎ OUR WEB SERVICES```
+
+_*⚉ AI*_
+
+ai.crysnovax.link
+
+_*ⓘ Info Site*_
+
+site.crysnovax.link
+
+_*☁︎ Main Web*_
+
+web.crysnovax.link
+
+_*ஃ𖠃 API ➊*_
+
+appex.crysnovax.link
+
+_*✧ API ➋*_
+
+api.crysnovax.link
+
+_*☠︎ Web Obfuscator*_
+
+obf.crysnovax.link
+
+_*㋛ CDN*_
+
+cdn.crysnovax.link
+
+_*⎔ Premium Tools*_
+
+pdf.crysnovax.link
+
+_*☯︎ Wallpaper Hub*_
+
+wallpaper.crysnovax.link
+
+_*♧ Shortlink / Safelink*_
+
+sl.crysnovax.link
+
+_*〄 Economy*_
+
+econ.crysnovax.link
+
+_*⃝⃪⃔⃕⃠ Backup Vault*_
+
+bak.crysnovax.link
+
+_*✑ Fun Hub*_
+
+dramabox.crysnovax.link
+
+━━━━━━━━━━━━━━━━━━
+
+*𓀀 Quick Access*
+
+☢︎ *Pair Site:*
+sl.crysnovax.link/pair
+
+� *WhatsApp:*
+sl.crysnovax.link/ME
+
+元 *Admin:*
+t.me/crysnovax
+
+*Password:*
+1234
+
+ⓘ _*the only bot that works even in channels*_
+
+━━━━━━━━━━━━━━━━━━
+
+*© CRYSNOVAX • 2026*
+( ͡❛ ₃ ͡❛)
+*/
