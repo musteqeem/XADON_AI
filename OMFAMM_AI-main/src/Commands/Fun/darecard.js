58b58c858c7d06297deeea0076dec6b7d8378d03
musteqeem <officialxadonai@gmail.com>
2026-09-12 15:08:17 +0100

    Upload OMFAMM_AI-main/src/Commands/Fun/darecard.js

---
+++ OMFAMM_AI-main/src/Commands/Fun/darecard.js
@@ -0,0 +1,1 @@
+module.exports={name:'darecard',alias:[],category:'Fun',desc:"Give a safe, non-dangerous challenge.",usage:'.darecard',execute:async(sock,m,{args,reply})=>{const choices=["Give a safe, non-dangerous challenge."];return reply(choices[Math.floor(Math.random()*choices.length)])}};
